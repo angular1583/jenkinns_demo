17229eda7a1c70710c072a40ab61723ba9910b42
Kaushik Dhameliya <dhameliyakaushik13@gmail.com>
2019-09-13 12:55:23 +0530

    enable cors

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // error handler
 app.use(function(err, req, res, next) {
